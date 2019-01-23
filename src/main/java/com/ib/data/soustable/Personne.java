d2c494cb58fedaef60663b60a0995676721504f6
C. Mourad Jaber <cmjaber@nativobject.com>
2019-01-23 11:53:47 +0100

    Autre strategy d'héritage

---
+++ src/main/java/com/ib/data/soustable/Personne.java
@@ -0,0 +1,49 @@
+package com.ib.data.soustable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "PERSONNE")
+public class Personne {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nom;
+	
+	private String prenom;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+}
